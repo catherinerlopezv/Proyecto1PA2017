9eb787fbdfe3810da3bc79048a24218386293bf9
catherinerlopezv <catherinerlopezv@gmail.com>
2017-10-04 20:40:30 -0600

    Add files via upload

---
+++ ConsoleApp1/Documentacion.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Documentacion
+    {
+        private int ley;
+        private string titulo;
+        private int fechaCreacion;
+        private string contenido;
+        private int numCopias;
+        private int copiasDisponibles;
+
+
+        //list<colaCopias>
+
+        public int Ley
+        {
+            get
+            {
+                return ley;
+            }
+            set
+            {
+                ley = value;
+            }
+        }
+
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+            set
+            {
+                titulo = value;
+            }
+        }
+
+
+        public int FechaCreacion
+        {
+            get
+            {
+                return fechaCreacion;
+            }
+            set
+            {
+                fechaCreacion= value;
+            }
+        }
+
+        public string Contenido
+        {
+            get
+            {
+                return contenido;
+            }
+            set
+            {
+                contenido = value;
+            }
+        }
+
+
+        public int NumCopias
+        {
+            get
+            {
+                return numCopias;
+            }
+            set
+            {
+                numCopias = value;
+            }
+        }
+
+        public int CopiasDisponibles
+        {
+            get
+            {
+                return copiasDisponibles;
+            }
+            set
+            {
+                copiasDisponibles = value;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Usuario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Usuario
+    {
+
+        private int numGrupo;
+        private string inUsuario;
+        private string clave;
+        private string nombre;
+        // private int asesores; list <Asesor> asesores; list <parlamentario> parlamentario;
+
+        public int NumGrupo
+        {
+            get
+            {
+                return numGrupo;
+            }
+            set
+            {
+                numGrupo = value;
+            }
+        }
+
+        public string InUsuario
+        {
+            get
+            {
+                return inUsuario;
+            }
+            set
+            {
+                inUsuario = value;
+            }
+        }
+
+        public string Clave
+        {
+            get
+            {
+                return clave;
+            }
+            set
+            {
+                clave = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+    }
+}
